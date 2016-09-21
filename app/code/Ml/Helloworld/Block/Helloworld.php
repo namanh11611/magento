d55e9b307e6e84647dcd0b356f5ff9113cee45bd
namanh11611 <conan11611@yahoo.com.vn>
2016-09-21 11:26:32 +0700

    Contructor

---
+++ app/code/Ml/Helloworld/Block/Helloworld.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ml\Helloworld\Block;
+
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+	public function _prepareLayout()
+	{
+		return parent::_prepareLayout();
+	}
+}
